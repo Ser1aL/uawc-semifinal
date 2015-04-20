1bd6baae945577b9b3729db9c18c3ed982c3e26f
Max Reznichenko <max.reznichenko@gmail.com>
2015-04-20 20:24:15 +0300

    Trying locales with quotes

--- bootstrap.sh
+++ bootstrap.sh
@@ -23,14 +23,14 @@ echo "======> locale before change====="
 locale
 echo "======changing====="
 cat >/etc/default/locale <<EOL
-LANG=en_US.UTF-8
+LANG="en_US.UTF-8"
 LANGUAGE=
-LC_CTYPE=en_US.UTF-8
-LC_NUMERIC=en_US.UTF-8
-LC_TIME=en_US.UTF-8
-LC_COLLATE=en_US.UTF-8
-LC_MONETARY=en_US.UTF-8
-LC_MESSAGES=en_US.UTF-8
+LC_CTYPE="en_US.UTF-8"
+LC_NUMERIC="en_US.UTF-8"
+LC_TIME="en_US.UTF-8"
+LC_COLLATE="en_US.UTF-8"
+LC_MONETARY="en_US.UTF-8"
+LC_MESSAGES="en_US.UTF-8"
 LC_PAPER="en_US.UTF-8"
 LC_NAME="en_US.UTF-8"
 LC_ADDRESS="en_US.UTF-8"
