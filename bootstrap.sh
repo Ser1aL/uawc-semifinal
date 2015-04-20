e62f4d61b962ce0e2f2361b9a12061386582a5b0
Max Reznichenko <max.reznichenko@gmail.com>
2015-04-20 20:46:45 +0300

    Removed debugging of bootstrap.sh file

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,36 +18,11 @@ rvm use ruby-2.2.1@uawc-semifinal --default --create
 
 bundle install
 
-echo "======Debugging locales====="
-echo "======> locale before change====="
-locale
-echo "======changing====="
 cat >/etc/default/locale <<EOL
-LANG="en_US.UTF-8"
-LANGUAGE=
-LC_CTYPE="en_US.UTF-8"
-LC_NUMERIC="en_US.UTF-8"
-LC_TIME="en_US.UTF-8"
-LC_COLLATE="en_US.UTF-8"
-LC_MONETARY="en_US.UTF-8"
-LC_MESSAGES="en_US.UTF-8"
-LC_PAPER="en_US.UTF-8"
-LC_NAME="en_US.UTF-8"
-LC_ADDRESS="en_US.UTF-8"
-LC_TELEPHONE="en_US.UTF-8"
-LC_MEASUREMENT="en_US.UTF-8"
-LC_IDENTIFICATION="en_US.UTF-8"
-LC_ALL=
+LC_ALL="en_US.UTF-8"
 EOL
 source /etc/default/locale
-echo "======> locale after change====="
-locale
-echo "======reloading shell====="
-# hard reload the shell for locale changes to take effect
-/bin/bash
-echo "======> locale after reload====="
-locale
-echo "============================"
+
 echo 'Starting services'
 cd /opt/uawc-semifinal && ./restart_server.sh
 echo 'Puma started!'
